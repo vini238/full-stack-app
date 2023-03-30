0fca2c0027afb63b9abeb23087442e65985f3a31
Vincent Meka <vmeka@ltu.edu>
2023-03-30 18:33:42 -0400

    created dockerfile and updated service test

--- employeeApp/src/test/kotlin/com/employee/demo/service/EmployeeServiceTest.kt
+++ employeeApp/src/test/kotlin/com/employee/demo/service/EmployeeServiceTest.kt
@@ -9,9 +9,10 @@ import org.junit.jupiter.api.extension.ExtendWith
 import org.mockito.ArgumentMatchers.any
 import org.mockito.InjectMocks
 import org.mockito.Mock
+import org.mockito.Mockito.verify
 import org.mockito.Mockito.`when`
 import org.mockito.junit.jupiter.MockitoExtension
-import java.util.UUID
+import java.util.*
 
 
 @ExtendWith(MockitoExtension::class)
@@ -45,18 +46,21 @@ class EmployeeServiceTest {
     }
 
     @Test
-    fun `given valid id and customer, when calling updateEmployee(), then update customer`() {
+    fun `given valid id and updated employee, when calling updateEmployee(), then update employee`() {
         val employeeId = UUID.randomUUID()
-        val employee = Employee(employeeId, "John", "Doe")
-        `when`(employeeRepository.existsById(employeeId)).thenReturn(true)
-        `when`(employeeRepository.save(any(Employee::class.java))).thenReturn(employee)
+        val existingEmployee = Employee(employeeId, "John", "Doe")
+        val updatedEmployee = Employee(employeeId, "Jane", "Doe")
+        `when` (employeeRepository.findById(employeeId)).thenReturn(Optional.of(existingEmployee))
+        `when` (employeeRepository.save(any(Employee::class.java))).thenReturn(updatedEmployee)
 
-        val result = employeeService.updateEmployee(employeeId, employee)
+        val result = employeeService.updateEmployee(employeeId, updatedEmployee)
 
-        assertEquals(employee, result)
+        assertEquals(updatedEmployee, result)
+        verify(employeeRepository).findById(employeeId)
+        verify(employeeRepository).save(updatedEmployee)
     }
 
-    @Test
+        @Test
     fun `given valid id, when calling deleteEmployee(), then delete employee`() {
         val employeeId = UUID.randomUUID()
         `when`(employeeRepository.existsById(employeeId)).thenReturn(true)
