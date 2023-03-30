3c3e9b877413c5dbccff7d819451cf56fca674d2
Vincent Meka <vmeka@ltu.edu>
2023-03-30 13:54:28 -0400

    minor frontend changes

--- frontend/employee-front-end/src/components/GetEmployee.js
+++ frontend/employee-front-end/src/components/GetEmployee.js
@@ -1,14 +1,14 @@
 import React, { useState, useEffect } from 'react';
 import "../style/GetEmployee.css";
 
-function EmployeeList(props) {
+function EmployeeList() {
   const [employees, setEmployees] = useState([]);
 
   useEffect(() => {
     fetch('http://localhost:8080/employees')
       .then(response => response.json())
       .then(data => setEmployees(data));
-  }, [props.shouldUpdateList]);
+  }, []);
 
   return (
     <table className="employee-table">
